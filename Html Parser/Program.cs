969ebd6274f72518fe47807719c6733c37a409f0
paul.cirstean <paul.cirstean@LAPTOP-52>
2021-10-28 18:39:22 +0300

    Add project files.

---
+++ Html Parser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Html_Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
